6dd82f45b80d8c0e88045d037c4d38997bb3e676
Pallavi <pallavi.panchal2989@gmail.com>
2022-10-05 13:46:47 +0530

    Convert string to lowercase

---
+++ src/string/school/ConvertStringToLowercase.java
@@ -0,0 +1,11 @@
+package string.school;
+
+public class ConvertStringToLowercase {
+    public static void main(String[] args) {
+        System.out.println(toLower("ABCddE"));
+    }
+
+    static String toLower(String S) {
+        return S.toLowerCase();
+    }
+}
