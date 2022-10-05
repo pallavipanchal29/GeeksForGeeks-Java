77c81057e72b2b388dfb13ec1d583a8d9a73f769
Pallavi <pallavi.panchal2989@gmail.com>
2022-10-05 11:45:22 +0530

    Lower case to upper case

---
+++ src/string/school/LowerCaseToUpperCase.java
@@ -0,0 +1,11 @@
+package string.school;
+
+public class LowerCaseToUpperCase {
+    public static void main(String[] args) {
+        System.out.println(to_upper("geeks"));
+    }
+
+    static String to_upper(String str) {
+        return str.toUpperCase();
+    }
+}
