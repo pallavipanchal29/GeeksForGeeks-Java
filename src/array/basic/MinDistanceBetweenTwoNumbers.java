824e8b399eaea0eef5cc6e15c21346da7defa90b
Pallavi <pallavi.panchal2989@gmail.com>
2022-10-08 12:35:21 +0530

    Minimum distance between two numbers

---
+++ src/array/basic/MinDistanceBetweenTwoNumbers.java
@@ -0,0 +1,62 @@
+package array.basic;
+
+import java.util.HashMap;
+
+public class MinDistanceBetweenTwoNumbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 2};
+        System.out.println(minDistHashmap(arr, arr.length, 1, 2));
+    }
+
+    //o(nsquare)
+    static int minDist(int[] a, int n, int x, int y) {
+        int minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                int j = i;
+                while (j < a.length) {
+                    if (a[j] == y) {
+                        minDistance = Math.min(j - i, minDistance);
+                    }
+                    j++;
+                }
+            }
+            if (a[i] == y) {
+                int j = i;
+                while (j < a.length) {
+                    if (a[j] == x) {
+                        minDistance = Math.min(j - i, minDistance);
+                    }
+                    j++;
+                }
+            }
+        }
+        if (minDistance == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return minDistance;
+    }
+
+    //Not working correctly - refactor
+    static int minDistHashmap(int[] a, int n, int x, int y) {
+        int min = Integer.MAX_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(a[i])) {
+                if (a[i] == x && map.containsKey(y))
+                    min = Math.min(Math.abs(i - map.get(y)), min);
+                if (a[i] == y && map.containsKey(x))
+                    min = Math.min(Math.abs(i - map.get(x)), min);
+                map.put(a[i], i);
+            } else if (a[i] == x) {
+                if (map.containsKey(y))
+                    min = Math.min(i - map.get(x), min);
+            } else if (a[i] == y) {
+                if (map.containsKey(x))
+                    min = Math.min(i - map.get(y), min);
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+
+}
