fd99083afed3360ac7f2a2275594bdd8ec6a0d4c
Pallavi <pallavi.panchal2989@gmail.com>
2022-10-08 12:01:43 +0530

    Maximize sum of array

--- src/array/basic/MaximizeSumOfArray.java
+++ src/array/basic/MaximizeSumOfArray.java
@@ -9,11 +9,11 @@ public class MaximizeSumOfArray {
     }
 
     static int Maximize(int[] arr, int n) {
-        int sum = 0;
         Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            sum = sum + arr[i] * i;
+        long sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + (long) arr[i] * i;
         }
-        return sum;
+        return (int) (sum % (1000000007));
     }
 }
