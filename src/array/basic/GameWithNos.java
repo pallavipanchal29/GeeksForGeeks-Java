36dbd7e63ef9a205303f5ad5dc33b4e3d9cc2656
Pallavi <pallavi.panchal2989@gmail.com>
2022-11-30 15:32:24 +0530

    Game with nos

---
+++ src/array/basic/GameWithNos.java
@@ -0,0 +1,19 @@
+package array.basic;
+
+public class GameWithNos {
+    public static void main(String[] args) {
+        int[] arr = {10, 11, 1, 2, 3};
+        int[] res = game_with_number(arr, arr.length);
+
+        for (int i = 0; i < res.length; i++)
+            System.out.print(res[i] + " ");
+    }
+
+    // Function for finding maximum and value pair
+    public static int[] game_with_number(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i] ^ arr[i + 1];
+        }
+        return arr;
+    }
+}
