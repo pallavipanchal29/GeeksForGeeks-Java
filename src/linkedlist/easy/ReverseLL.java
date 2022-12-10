5eb6c2c3da443586ff52299866e9c0f07554214f
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-10 16:05:02 +0530

    Reverse LL

---
+++ src/linkedlist/easy/ReverseLL.java
@@ -0,0 +1,21 @@
+package linkedlist.easy;
+
+import linkedlist.Node;
+
+public class ReverseLL {
+    //Function to reverse a linked list.
+    static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
