b861b97c58b656f6663debd60256e006e4f9a63f
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-18 10:33:22 +0530

    Construct BT from array

---
+++ src/tree/binarytree/ConstructBTFromArray.java
@@ -0,0 +1,15 @@
+package tree.binarytree;
+
+import tree.Node;
+
+public class ConstructBTFromArray {
+    public Node insertLevelOrder(int[] arr, int i) {
+        Node root = null;
+        if (i < arr.length) {
+            root = new Node(arr[i]);
+            root.left = insertLevelOrder(arr, 2 * i + 1);
+            root.right = insertLevelOrder(arr, 2 * i + 2);
+        }
+        return root;
+    }
+}
