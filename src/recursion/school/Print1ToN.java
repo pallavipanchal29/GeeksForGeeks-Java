6a51ab51752000da178de8c578f6727e8f84ebc1
Pallavi <pallavi.panchal2989@gmail.com>
2022-12-18 12:42:00 +0530

    Print 1 to N

--- src/recursion/school/Print1ToN.java
+++ src/recursion/school/Print1ToN.java
@@ -3,14 +3,14 @@ package recursion.school;
 public class Print1ToN {
     public static void main(String[] args) {
         printNos(10);
-        for (int i = 1; i <= 10; i++)
-            System.out.print(i + " ");
     }
 
     public static void printNos(int N) {
-        if (N == 1)
+        if (N == 0)
             return;
-        else
+        else {
             printNos(N - 1);
+            System.out.print(N + " ");
+        }
     }
 }
